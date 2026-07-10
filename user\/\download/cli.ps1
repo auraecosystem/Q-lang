1e60d1def43d971906d164a82255f7cd429bccf4
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-10 02:08:51 +0100

    Create cli.ps1

---
+++ user\/\download/cli.ps1
@@ -0,0 +1,3 @@
+expand {PATH TO THE PACKAGE} -F:* {PATH TO THE DESTINATION FOLDER}
+Microsoft.WebView2.FixedVersionRuntime.114.0.1823.79.x86.cab
+rename-item "C:\src\MySolution\Microsoft.WebView2.FixedVersionRuntime.114.0.1823.79.x86" "C:\src\MySolution\Runtimex86"
