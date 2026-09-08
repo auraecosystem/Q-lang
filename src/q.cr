bc1cd2c2177e7cb06cb6f82ce5e8ccfd14bec94c
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-08 23:50:41 +0100

    Update q.cr

--- src/q.cr
+++ src/q.cr
@@ -1,1 +1,11 @@
-*/*
+# src/q.cr
+
+module Q
+  VERSION = "0.1.0"
+
+  def self.run(source : String)
+    tokens = Lexer.tokenize(source)
+    ast = Parser.parse(tokens)
+    Runtime.execute(ast)
+  end
+end
